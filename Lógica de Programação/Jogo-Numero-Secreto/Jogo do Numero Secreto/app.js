c0e59163b44a1853ca19e54125b312123569aa9a
1thallo <ithalloflu@gmail.com>
2025-01-31 21:38:24 -0300

    add JogoNS and README

---
+++ Lógica de Programação/Jogo-Numero-Secreto/Jogo do Numero Secreto/app.js
@@ -0,0 +1,66 @@
+let listaNumerosSorteados = [];
+let numeroMaximoTentativas = 100;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto){
+   let campo = document.querySelector(tag);
+   campo.innerHTML = texto;
+}
+
+function gerarNumeroAleatorio(){
+   let numeroSorteado = parseInt(Math.random() * numeroMaximoTentativas + 1);
+   let quantidadeElementos = listaNumerosSorteados.length;
+
+   if(quantidadeElementos == numeroMaximoTentativas){
+      listaNumerosSorteados = [];
+   }
+
+   if(listaNumerosSorteados.includes(numeroSorteado)){
+      return gerarNumeroAleatorio(); 
+   }
+      else {
+         listaNumerosSorteados.push(numeroSorteado);
+         console.log(listaNumerosSorteados);
+         return numeroSorteado;
+      }
+}
+
+
+function exibirMensagemInicial(){
+   exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+   exibirTextoNaTela('p', `Tente advinhar o número secreto, entre 1 e ${numeroMaximoTentativas}.`);
+}
+
+exibirMensagemInicial();
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', `Tente advinhar o número secreto, entre 1 e ${numeroMaximoTentativas}.`);
+
+function verificarChute(){
+   let chute = parseInt(document.querySelector('input').value);
+   console.log(`Chute: ${chute}\nNúmero Secreto: ${numeroSecreto}\nChute é igual ao número secreto? ${chute == numeroSecreto}\nQuantidade de tentativas: ${tentativas}`);
+
+   let palavraTentativas = tentativas == 1 ? 'tentativa' : 'tentativas';
+
+   chute == numeroSecreto ? 
+   (exibirTextoNaTela('h1', 'Parabéns, você acertou!'), exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}! :D`), document.getElementById('reiniciar').removeAttribute('disabled'))//numero certo**
+   : chute > numeroSecreto ? // condicao
+   exibirTextoNaTela('p', `O número secreto é menor que ${chute}`)
+   : exibirTextoNaTela('p', `O número secreto é maior que ${chute}`)
+   tentativas++;  
+   limparCampo();
+}
+
+function limparCampo(){
+   chute = document.querySelector('input');
+   chute.value = '';
+}
+
+function reiniciarJogo(){
+   numeroSecreto = gerarNumeroAleatorio();
+   limparCampo();
+   tentativas = 1;
+   exibirMensagemInicial();
+   document.getElementById('reiniciar').setAttribute('disabled', 'disabled');
+}
---
+++ Lógica de Programação/Jogo-Numero-Secreto/README.md
@@ -0,0 +1,59 @@
+# 🎯 Jogo do Número Secreto
+
+Bem-vindo ao **Jogo do Número Secreto**! Este é um projeto desenvolvido como parte do curso de **Lógica de Programação com JavaScript** no programa **ONE - Oracle Next Education**. O jogo foi aprimorado ao longo do aprendizado, incorporando novas funcionalidades e melhorias.
+
+## 📌 Sobre o Jogo
+
+O Jogo do Número Secreto é um desafio onde o jogador precisa adivinhar um número aleatório dentro de um intervalo definido. A cada tentativa, o jogo informa se o palpite é maior ou menor do que o número secreto, ajudando o jogador a encontrar a resposta correta.
+
+## 🚀 Funcionalidades
+
+- Geração de um número aleatório dentro de um intervalo específico
+- Entrada de dados do usuário para tentativa de acerto
+- Dicas informando se o número secreto é maior ou menor que o palpite
+- Contador de tentativas
+- Mensagens dinâmicas de vitória
+- Opção para reiniciar o jogo
+
+## 🛠️ Tecnologias Utilizadas
+
+- **HTML**: Estrutura básica do jogo
+- **CSS**: Estilização para uma interface mais agradável
+- **JavaScript**: Lógica do jogo e interatividade
+
+## 📖 Como Jogar
+
+1. O jogo gera um número secreto aleatório.
+2. O jogador digita um número e clica no botão de "Chutar"
+3. O jogo informa se o número digitado é maior, menor ou igual ao número secreto.
+4. O jogador continua tentando até acertar.
+5. Ao acertar, o jogo exibe uma mensagem de vitória e a opção de jogar novamente.
+
+## 💡 Aprendizados
+
+Durante o desenvolvimento deste projeto, foram explorados conceitos fundamentais de lógica de programação, como:
+
+- Variáveis e tipos de dados
+- Estruturas de controle (condicionais e loops)
+- Manipulação do DOM com JavaScript
+- Funções e eventos
+
+## 📌 Como Executar o Projeto
+
+1. Acesse o jogo online através do seguinte link: [Jogo do Número Secreto](https://jogonumerosecreto-steel.vercel.app)
+
+ou Faça o download / clone este repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/jogo-numero-secreto.git
+   ```
+2. Abra o arquivo `index.html` em qualquer navegador moderno.
+3. Divirta-se jogando! 🎮
+
+## 📜 Licença
+
+Este projeto foi desenvolvido para fins educacionais e está disponível sob a licença MIT.
+
+---
+
+✨ **Divirta-se jogando e continue aprimorando suas habilidades em JavaScript!** 🚀
+
